1ce8c1530d34ce8dec5770394bc469b3d4e49bdb
shifa <shifanisatuz092GMAIL.com>
2023-10-16 12:26:28 +0700

    mengumpulkan tugas

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama:Shifa Fanisatuz Zahra\n\n";
+    cout << "NPM : 2310631170049\n\n";
+    cout << "Kelas : A - Informatika"<<endl<<endl;
+    cout << "Cita-cita: Rich Aunty" << endl<<endl;
+    cout << "Motivasi masuk Informatika : Dari tokoh novel yang dibaca"<<endl;
+    return 0;
+}
